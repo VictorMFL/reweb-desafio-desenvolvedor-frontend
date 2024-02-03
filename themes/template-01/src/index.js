191b35fcca43376dd570f2ed8176c1a63ee2b7f9
VictorMFL <vm859222@gmail.com>
2024-02-03 16:14:02 -0300

    feat: add form and banner 01

---
+++ themes/template-01/src/index.js
@@ -0,0 +1,43 @@
+let data;
+
+// executa a requisição para pegar os dados do aruivo indes.json
+async function get() {
+  try {
+    const response = await fetch("../source/output/index.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    data = await response.json();
+    document.title = data.texts.title;
+
+    console.log(data);
+  } catch (error) {
+    console.log("Ocorreu um erro ao pegar os dados do arquivo json.");
+  }
+}
+
+get().then(() => {
+  // form
+  const inputName = document.querySelector("#name");
+  const inputEmail = document.querySelector("#email");
+  const inputCPF = document.querySelector("#cpf");
+  const inputPhone = document.querySelector("#telefone");
+  const areaMesage = document.querySelector("#imensagem");
+  const privacyPolicy = document.querySelector("#privacidade");
+  const buttonSubmit = document.querySelector("#button-submit");
+
+  inputName.placeholder = data.form.banner_01.fields[1].placeholder;
+  inputEmail.placeholder = data.form.banner_01.fields[2].placeholder;
+  inputCPF.placeholder = "CPF";
+  inputPhone.placeholder = data.form.banner_01.fields[3].placeholder;
+  areaMesage.placeholder = data.form.banner_01.fields[4].placeholder;
+  privacyPolicy.innerHTML = data.form.banner_01.settings.privacy_policy.label;
+  buttonSubmit.value = data.form.banner_01.settings.button;
+
+  // resposta do form
+  const sentSucces = document.querySelector("#sent-success");
+  const errorPost = document.querySelector("#error-post");
+
+  sentSucces.innerHTML = data.form.banner_01.settings.message_success;
+  errorPost.innerHTML = data.form.banner_01.settings.message_error;
+});
